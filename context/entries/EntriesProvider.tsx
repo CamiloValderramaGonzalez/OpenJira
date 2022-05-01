a7b69c2294d12c6f316f66f9fb86d3b78cae696d
Camilo Valderrama <camilovalderrama@gmail.com>
2022-05-01 11:51:03 -0300

    Se agrego funcionalidad de mongoDB

--- context/entries/EntriesProvider.tsx
+++ context/entries/EntriesProvider.tsx
@@ -9,29 +9,7 @@ export interface EntriesState {
 }
 
 const Entries_INITIAL_STATE: EntriesState = {
-    entries: [
-        {
-            _id: uuidv4(),
-            description:
-                "Pendiente lorem dasfhjasdk dlsk sdlksa aslk31dmdps sdksl ",
-            status: "pending",
-            createAt: Date.now(),
-        },
-        {
-            _id: uuidv4(),
-            description:
-                "Terminada lorem dasfhjasdk dlsk sdlksa aslk31dmdps sdksl ",
-            status: "finished",
-            createAt: Date.now() - 1000000,
-        },
-        {
-            _id: uuidv4(),
-            description:
-                "En progreso lorem dasfhjasdk dlsk sdlksa aslk31dmdps sdksl ",
-            status: "in-progres",
-            createAt: Date.now() - 2000000,
-        },
-    ],
+    entries: [],
 };
 
 interface Props {
---
+++ database/seed-data.ts
@@ -0,0 +1,32 @@
+interface ISeedData {
+    entries: ISeedEntry[];
+}
+
+interface ISeedEntry {
+    description: string;
+    status: string;
+    createAt: number;
+}
+
+export const seedData: ISeedData = {
+    entries: [
+        {
+            description:
+                "Pendiente lorem dasfhjasdk dlsk sdlksa aslk31dmdps sdksl ",
+            status: "pending",
+            createAt: Date.now(),
+        },
+        {
+            description:
+                "Terminada lorem dasfhjasdk dlsk sdlksa aslk31dmdps sdksl ",
+            status: "finished",
+            createAt: Date.now() - 1000000,
+        },
+        {
+            description:
+                "En progreso lorem dasfhjasdk dlsk sdlksa aslk31dmdps sdksl ",
+            status: "in-progres",
+            createAt: Date.now() - 2000000,
+        },
+    ],
+};
